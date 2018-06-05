8a6205b7e1354c513a1434473712d38bf011e2ff
superasperatus <39364220+superasperatus@users.noreply.github.com>
2018-06-05 20:35:07 +0200

    changing the image location

--- face_detector.py
+++ face_detector.py
@@ -2,7 +2,7 @@ import cv2 #imports cv to work with
 
 face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_default.xml") #creates a cascade element; takes default settings from Haar cascade xml file
 
-img=cv2.imread("s-p-b-bez-fd.jpg") #reads the image where we want to find the face
+img=cv2.imread("file-tbd-tbd.jpg") #reads the image where we want to find the face
 gray_img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #converts the image to a grey version
 
 faces=face_cascade.detectMultiScale(gray_img,
